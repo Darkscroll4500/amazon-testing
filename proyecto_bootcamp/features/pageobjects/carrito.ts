675671af11904dfc1c713920d300f1ab9d00ea57
nattury <natliegash@gmail.com>
2023-10-18 14:13:13 -0500

    Failed test of requirement 4

---
+++ proyecto_bootcamp/features/pageobjects/carrito.ts
@@ -0,0 +1,33 @@
+import { $, $$ } from '@wdio/globals';
+
+class Carrito {
+    public get productsInCart() {
+        return $$('.sc-item-content-group'); // Selecciona todos los elementos que representan productos en el carrito
+    }
+
+    public get subtotal() {
+        return $('#sc-subtotal-amount-buybox').getText(); // Selector para el subtotal del carrito
+    }
+
+    public async validarProductosEnCarrito(): Promise<{ productCount: number, subtotal: string, sonProductosDiferentes: boolean }> {
+        const products: any = await this.productsInCart; // Usar 'any' para evitar problemas de tipo
+        const uniqueProducts = new Set(products.map((product: any) => product.getText()));
+
+        return {
+            productCount: products.length,
+            subtotal: await this.subtotal,
+            sonProductosDiferentes: uniqueProducts.size === products.length,
+        };
+    }
+
+    public async irAlCarrito() {
+        const carritoElement: any = await $('a[aria-label="3 artículos en el carrito"]'); // Usar 'any' para evitar problemas de tipo
+        await carritoElement.click();
+    }
+
+    public get subtotalRaw() {
+        return $('#sc-subtotal-amount-buybox .a-price').getText();
+    }
+}
+
+export default new Carrito();
--- proyecto_bootcamp/features/step-definitions/steps.ts
+++ proyecto_bootcamp/features/step-definitions/steps.ts
@@ -4,6 +4,7 @@ import { expect, $, browser } from '@wdio/globals'
 import RegisterPage from '../pageobjects/pag.registro';
 import LoginPage from '../pageobjects/pag.iniciar.sesion';
 import SecurePage from '../pageobjects/secure.page';
+import Carrito from '../pageobjects/carrito';
 
 const pages = {
     login: RegisterPage
@@ -36,3 +37,34 @@ When(/^Yo inicio sesion con (.*), (.*)$/, async (correo, contrasena) => {
     await LoginPage.iniciarSesion('miboxa5375@klanze.com', '12345Jose');
     await browser.pause(200000);
 });
+
+
+//carrito
+
+When(/^El usuario va al carrito de compras$/, async () => {
+    await Carrito.irAlCarrito();
+});
+
+Then(/^El carrito debe contener al menos 3 productos$/, async () => {
+    const { productCount } = await Carrito.validarProductosEnCarrito();
+    expect(productCount).toBeGreaterThanOrEqual(3);
+});
+
+Then(/^El subtotal debe corresponder a la suma de los 3 productos$/, async () => {
+    const { subtotal } = await Carrito.validarProductosEnCarrito();
+    const subtotalRaw = await Carrito.subtotalRaw;
+    const calculatedSubtotal = parseFloat(subtotalRaw.replace('US$', '').trim()); // Convierte el subtotal sin formato a un número
+
+    expect(Number(subtotal)).toEqual(calculatedSubtotal);
+});
+
+
+Then(/^Los 3 productos en el carrito deben ser diferentes$/, async () => {
+    const { sonProductosDiferentes } = await Carrito.validarProductosEnCarrito();
+    expect(sonProductosDiferentes).toBe(true);
+});
+
+
+
+
+
